b5b976c841a559a2682a2f9fd46f5238c06c2c65
Sheela Adhikari <71367240+sheelaadhikari@users.noreply.github.com>
2022-01-20 18:04:54 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// function minusSeven(num) {
+//     return num - 7;
+
+// }
+// console.log(minusSeven(10));
+
+var changed = 0;
+
+function change(num) {
+    return (num + 5) / 3;
+
+}
+changed = change(10);
+console.log(changed);
